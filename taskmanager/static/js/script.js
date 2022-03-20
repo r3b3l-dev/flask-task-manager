90d46c26a21820eecd43297ed61e5fa1851680b6
r3b3l-dev <wayne.manning@tuta.io>
2022-03-20 21:12:36 +0000

    Complete add task funtionality

--- taskmanager/static/js/script.js
+++ taskmanager/static/js/script.js
@@ -1,12 +1,20 @@
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener("DOMContentLoaded", function() {
   // sidenav initialization
-  let sidenav = document.querySelectorAll('.sidenav');
+  let sidenav = document.querySelectorAll(".sidenav");
   M.Sidenav.init(sidenav);
-});
 
-
-// modal trigger
-document.addEventListener('DOMContentLoaded', function() {
+  // modal trigger
   let modal = document.querySelectorAll('.modal');
   M.Modal.init(modal);
+
+  // datepicker initialization
+  let datepicker = document.querySelectorAll(".datepicker");
+  M.Datepicker.init(datepicker, {
+      format: "dd mmmm, yyyy",
+      i18n: {done: "Select"}
+  });
+
+  // select initialization
+  let selects = document.querySelectorAll("select");
+  M.FormSelect.init(selects);
 });
