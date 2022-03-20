ff7fe9debc942003072e70a00dd1c34205aa6191
r3b3l-dev <wayne.manning@tuta.io>
2022-03-20 17:56:23 +0000

    Add defensive programming modal which triggers upon delete button click

--- taskmanager/static/js/script.js
+++ taskmanager/static/js/script.js
@@ -1,5 +1,12 @@
 document.addEventListener('DOMContentLoaded', function() {
-    // sidenav initialization
-    let sidenav = document.querySelectorAll('.sidenav');
-    M.Sidenav.init(sidenav);
-  });
+  // sidenav initialization
+  let sidenav = document.querySelectorAll('.sidenav');
+  M.Sidenav.init(sidenav);
+});
+
+
+// modal trigger
+document.addEventListener('DOMContentLoaded', function() {
+  let modal = document.querySelectorAll('.modal');
+  M.Modal.init(modal);
+});
